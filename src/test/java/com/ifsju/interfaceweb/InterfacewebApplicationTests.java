f93e3314d823d404160029231abafad6fde2bf09
Jeong-Rae <kkwjdfo@gmail.com>
2023-08-22 17:44:03 +0900

    [ADD] init

---
+++ src/test/java/com/ifsju/interfaceweb/InterfacewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsju.interfaceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfacewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
