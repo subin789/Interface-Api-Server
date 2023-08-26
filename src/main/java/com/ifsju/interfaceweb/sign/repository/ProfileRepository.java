6e1e3be0944e3d66c793ed18db296d94ca5507aa
Jeong-Rae <kkwjdfo@gmail.com>
2023-08-27 00:36:26 +0900

    [DEL] clear..

--- src/main/java/com/ifsju/interfaceweb/sign/repository/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ifsju.interfaceweb.sign.repository;
-
-import com.ifsju.interfaceweb.sign.entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-}
--- src/main/java/com/ifsju/interfaceweb/sign/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ifsju.interfaceweb.sign.repository;
-
-import com.ifsju.interfaceweb.sign.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
